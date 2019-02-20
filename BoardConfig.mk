cc97e298e549a4cc3e932d9e5c8aba0aee5e6361
Mukesh Ojha <mojha@codeaurora.org>
2019-02-20 12:24:26 +0530

    trinket: Set configuration to add dtbo in recovery image
    Set configuration to add dtbo in recovery image.
    
    Change-Id: I1d39dc88fbb614b5aabe5525aecd38453bcc8aaf

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,6 +36,10 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/qcom/common
 USE_OPENGL_RENDERER := true
 BOARD_USE_LEGACY_UI := true
 
+# Set Header version for bootimage
+BOARD_BOOTIMG_HEADER_VERSION := 1
+BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
+
 ifeq ($(ENABLE_AB), true)
 # Defines for enabling A/B builds
 AB_OTA_UPDATER := true
@@ -62,6 +66,8 @@ ifeq ($(BOARD_AVB_ENABLE), true)
    BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
    BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 endif
+# Enable DTBO for recovery image
+BOARD_INCLUDE_RECOVERY_DTBO := true
 endif
 
 BOARD_USES_METADATA_PARTITION := true
