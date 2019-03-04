753a7f299ff7c282e308aeaff6aa7db441a3c8a2
Honglei Shen <hshen@codeaurora.org>
2019-03-04 17:15:01 +0800

    trinket: disable fingerprint feature
    To disable CTS test fingerprint error message on trinket.
    
    Change-Id: I7998a4f37237e54bc52a33636333146546a7958a
    Signed-off-by: Honglei Shen <hshen@codeaurora.org>

--- trinket.mk
+++ trinket.mk
@@ -158,10 +158,6 @@ endif
 PRODUCT_PACKAGES += \
     libhealthd.msm
 
-# Fingerprint feature
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml \
-
 # Adding vendor manifest
 PRODUCT_COPY_FILES += \
     device/qcom/$(TRINKET)/manifest.xml:$(TARGET_COPY_OUT_VENDOR)/manifest.xml
