7c982cead5ad8748e321cc1f923423d895af3d5d
Samyak Jain <samyjain@codeaurora.org>
2019-06-19 16:16:13 +0530

    trinket: declare midi feature support
    Declare MIDI feature support.
    
    Change-Id: I92327c74d751401e5373864c23da857d997464d6

--- trinket.mk
+++ trinket.mk
@@ -204,7 +204,7 @@ PRODUCT_PACKAGES += \
 
 # MIDI feature
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml
+    frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
 #servicetracker HAL
 PRODUCT_PACKAGES += \
