e1af939fb6d42f8b4eebdcec0137b0c1bac83254
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-10-03 14:48:22 +0530

    ginkgo: Inherit 4GB dalvik heap configration

--- ginkgo.mk
+++ ginkgo.mk
@@ -91,13 +91,8 @@ TARGET_VENDOR := xiaomi
 TARGET_DEFINES_DALVIK_HEAP := true
 $(call inherit-product, device/qcom/vendor-common/common64.mk)
 
-#Inherit all except heap growth limit from phone-xhdpi-2048-dalvik-heap.mk
-PRODUCT_PROPERTY_OVERRIDES  += \
-    dalvik.vm.heapstartsize=8m \
-    dalvik.vm.heapsize=512m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=512k \
-    dalvik.vm.heapmaxfree=8m
+#Inherit all dalvik properties for 4GB device
+$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
 PRODUCT_NAME := ginkgo
 PRODUCT_DEVICE := ginkgo
