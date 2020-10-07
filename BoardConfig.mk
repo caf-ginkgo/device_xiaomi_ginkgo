733f22219a1fba3f5e6a35eda77f913ef6c0eb9e
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-10-07 13:55:44 +0530

    ginkgo: Switch selinux to enforcing
    Android 11? Yes
    CAF? Haha yes
    User build? Sure, why not
    Release Keys? Yeah
    Enforcing? Hell yeah!

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -173,7 +173,7 @@ BOARD_VENDOR_KERNEL_MODULES += $(shell ls $(KERNEL_MODULES_OUT)/*.ko)
 TARGET_USES_ION := true
 TARGET_USES_NEW_ION_API := true
 TARGET_USES_QCOM_BSP := false
-BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 earlycon=msm_geni_serial,0x4a90000 loop.max_part=7 cgroup.memory=nokmem,nosocket androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 earlycon=msm_geni_serial,0x4a90000 loop.max_part=7 cgroup.memory=nokmem,nosocket
 
 BOARD_EGL_CFG := $(DEVICE_PATH)/egl.cfg
 
