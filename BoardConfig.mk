0e82228c4ce2e58d09573f7b20c788a4eccd1141
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-10-13 01:34:30 +0530

    ginkgo: Fix duplication of avbtool arguments
     * BoardConfig is included twice in a full CAF build, causing += declarations to
       duplicate their existing values

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -79,7 +79,7 @@ ifeq ($(BOARD_AVB_ENABLE), true)
    BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA4096
    BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
    BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
-   BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2 --set_hashtree_disabled_flag
+   BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS := --flag 2 --set_hashtree_disabled_flag
 endif
 # Enable DTBO for recovery image
 BOARD_INCLUDE_RECOVERY_DTBO := true
