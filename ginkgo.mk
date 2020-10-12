e9a9b6218641c7e1971861c3b04b231e4de3ab4a
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-10-13 01:29:01 +0530

    ginkgo: Disable FBE v2

--- ginkgo.mk
+++ ginkgo.mk
@@ -404,9 +404,8 @@ PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE:=true
 TARGET_MOUNT_POINTS_SYMLINKS := false
 
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.crypto.volume.filenames_mode = "aes-256-cts" \
-ro.crypto.allow_encrypt_override = true \
-ro.crypto.dm_default_key.options_format.version = 2
+    ro.crypto.volume.filenames_mode=aes-256-cts \
+    ro.crypto.allow_encrypt_override=true
 
 # Enable incremental FS feature
 PRODUCT_PROPERTY_OVERRIDES += ro.incremental.enable=1
