c7414709e10c25913ef3fa509a9b51b232cf0f09
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-10-22 21:31:42 +0530

    ginkgo: Import required NFC configs
     * libnfc-nxp_RF.conf from MIUI V12.0.1.0.QCXMIXM
     * libnfc-nxp.conf from LA.UM.9.11.r1-02100-NICOBAR.0 (renamed)

--- ginkgo.mk
+++ ginkgo.mk
@@ -137,6 +137,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
+# NFC
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/nfc/libnfc-nxp_RF.conf:$(TARGET_COPY_OUT_VENDOR)/libnfc-nxp_RF.conf \
+    $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
+
 # IR
 PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
